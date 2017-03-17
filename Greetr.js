0a19282feb2e059db8bf30bb8a3a5574d8a81b43
Jit Sarkar <jit.sarkar25@gmail.com>
2017-03-17 19:38:04 +0530

    added the methods to prototype

--- Greetr.js
+++ Greetr.js
@@ -6,7 +6,78 @@
         return new Greetr.init(firstname,lastname,language);
     };
     
-    Greetr.prototype = {};
+    var supportedLanguages = ['en','hi'];
+    
+    var greetings = {
+        en : "Hello",
+        hi : "Namaste"
+    };
+    
+    var formalgreetings = {
+      en : "Greetings",
+      hi : "Namaskar"
+    };
+    
+    var logMessages = {
+        en : "logged in",
+        hi : "logg hua hai "
+    };
+    
+    Greetr.prototype = {
+        
+        getfullname : function ()
+        {
+            return this.firstname + " " + this.lastname;
+        },
+        
+        informalgreet : function ()
+        {
+            return greetings[this.language] + " " + this.firstname;
+        },
+        
+        formalgreet :  function ()
+        {
+            return formalgreetings[this.language] + " " + this.getfullname();
+        },
+        
+        validatelang : function ()
+        {
+            if(supportedLanguages.indexOf(this.language) === -1)
+                {
+                    throw {
+                        name : "LanguageNotSupported",
+                        message : this.language + " is not supported"
+                    }
+                }
+        },
+        
+        greet : function(formal)
+        {
+            var msg;
+            if(formal)
+                {
+                    msg = this.formalgreet();
+                }
+            else
+                {
+                    msg = this.informalgreet();
+                }
+            
+            if(console)
+                {
+                    console.log(msg);
+                }
+            return this;
+        },
+        
+        setLang : function(lang)
+        {
+            this.language = lang;
+            this.validatelang();
+            return this;
+        }
+        
+    };
     
     Greetr.init = function (firstname , lastname , language)
     {
