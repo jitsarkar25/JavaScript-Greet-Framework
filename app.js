6c3fa24e8a85dc867fbdb311e66307784708bfd2
Jit Sarkar <jit.sarkar25@gmail.com>
2017-03-17 20:06:56 +0530

    added onclick event from the index.html

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-var g = G$("jit","sarkar");
-console.log(g);
+$("#loginBtn").click(function (){
+    
+    var firstname = $("#firstname").val();
+    var lastname = $("#lastname").val();
+    
+   var person =G$(firstname,lastname);
+    
+    var language = $("#Langselect").val();
+    
+    person.setLang(language);
+    
+    $("#maindiv").hide(true);
+    
+    person.HTMLgreet("#greeting",true);
+});
+
+
+
+
+
+
